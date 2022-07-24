1e10e8a637c488377eb1932aceb4d1f8f1c591d2
Paulina <pau.jastrzab@gmal.com>
2022-07-24 13:20:54 +0200

    account_creation

---
+++ src/main/java/AccountDetails.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class AccountDetails {
+    private String name;
+    private String accountNumber;
+    private String login;
+    private long balance;
+Scanner scanner = new Scanner(System.in);
+public void createAccount(){
+    System.out.println("Please type your name.");
+    name = scanner.next();
+    System.out.println("Please prvide your account number");
+    accountNumber = scanner.next();
+    System.out.println("Please type your login");
+    login = scanner.next();
+    System.out.println("Current account balance");
+    balance = scanner.nextLong();
+}
+    public void showAccount(){
+        System.out.println("Your account name is "+name);
+        System.out.println("Account number: "+accountNumber);
+        System.out.println("Your login name: "+login);
+        System.out.println("Current account balance: "+balance);
+    }
+}
---
+++ src/main/java/BankingApp.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class BankingApp {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        AccountDetails account = new AccountDetails();
+        System.out.println("Hi! Good to see you.");
+        account.createAccount();
+        int option;
+        do {
+            System.out.println("What would you like to do? \n 1.Your account information. \n 2. New deposit. \n 3. New withdrawal. \n 4. Exit ");
+            option = scanner.nextInt();
+            switch (option) {
+                case 1:
+                    account.showAccount();
+
+                    break;
+
+
+            }
+        }
+        while (option != 4);
+    }
+
+
+}
